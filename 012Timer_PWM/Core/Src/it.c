bfea59b713ea02f20504ecb1f70c0fe7683c7043
Sanjukta23 <sanjuktaa23@gmail.com>
2026-06-29 21:12:32 +0100

    update timer pwm

---
+++ 012Timer_PWM/Core/Src/it.c
@@ -0,0 +1,27 @@
+/*
+ * it.c
+ *
+ *  Created on: Jun 29, 2026
+ *      Author: SANJU
+ */
+
+#include "main.h"
+
+extern TIM_HandleTypeDef htimer2;
+
+/**
+  * @brief This function handles System tick timer.
+  */
+void SysTick_Handler (void)
+{
+	HAL_IncTick();
+	HAL_SYSTICK_IRQHandler();
+}
+
+/**
+  * @brief  This function handles TIM2 interrupt.
+  */
+void TIM2_IRQHandler(void)
+{
+  HAL_TIM_IRQHandler(&htimer2);
+}
