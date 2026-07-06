6521e197d29eb7e438a5e88b7f3954754f50faf6
Sanjukta23 <sanjuktaa23@gmail.com>
2026-07-06 13:53:34 +0100

    RTC Alarm Update

---
+++ 023RTC_Alarm_12h/Core/Inc/main.h
@@ -0,0 +1,17 @@
+/*
+ * main.h
+ *
+ *  Created on: Jul 6, 2026
+ *      Author: SANJU
+ */
+
+#include "stm32f4xx_hal.h"
+
+#define SYS_CLOCK_FREQ_50_MHZ   50
+#define SYS_CLOCK_FREQ_84_MHZ   84
+#define SYS_CLOCK_FREQ_120_MHZ  120
+
+#define TRUE  1
+#define FALSE 0
+
+void Error_handler(void);
